94d8a0ef58ead671bffb440c1f59df6752df58f4
simran s <sps022011@gmail.com>
2025-07-25 13:18:14 -0700

    Added basic setup for adding a task

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -10,14 +10,19 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+            HStack {
+                Text("To-Do List")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                Spacer()
+                Button {}
+                label: { (Text("+")) }
+            }//hstack
+            .padding()
+            Spacer()
+        }//vstack
+    }//body
+}//struct
 
 #Preview {
     ContentView()
---
+++ ToDoList/NewToDoView.swift
@@ -0,0 +1,30 @@
+//
+//  NewToDoView.swift
+//  ToDoList
+//
+//  Created by Scholar on 7/25/25.
+//
+
+import SwiftUI
+
+struct NewToDoView: View {
+    var body: some View {
+        VStack {
+            Text("Task Title")
+            TextField("Enter task description", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                Text("Is this important?")
+                Button {
+
+                } label: {
+                        Text("Save")
+                }//label
+            }//toggle
+        }//vstack
+        .padding()
+    }//body
+}//struct
+
+#Preview {
+    NewToDoView()
+}
